3a22cf381dd4fc81914df7a45c7ff0b7d47c14db
Aros <51179189+AliMZaini@users.noreply.github.com>
2019-06-28 16:16:01 +0100

    removed exe from root (can be found in TFTHelper-ItemCheatSheet

--- TFTHelper - Item Cheat Sheet/TFTHelper - Item Cheat Sheet - website/web/img/champions/Other/championbuttonsforhtml.py
+++
@@ -1,9 +0,0 @@
-import eel
-import io
-import os
-champions = os.listdir("..")
-
-print(champions)
-
-for champ in champions:
-    print("<button onclick=\"toggleChamp('"+champ+"')\"><img id= '"+champ+"' src=\"img/champions/"+champ+"\"></button>")
