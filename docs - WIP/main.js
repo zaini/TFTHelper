0031a1e4092d2f5199f347401446c2b34783f6d8
Aros <51179189+AliMZaini@users.noreply.github.com>
2019-06-29 00:02:03 +0100

    updated item sheet. no longer assumes 2 of every owned item

--- docs - WIP/main.js
+++ docs - WIP/main.js
@@ -101,7 +101,8 @@ function selectItem(item){
 		//Updates the sheet for the selected items
 		updateSheet(item, null);
 
-	};
+	}
+
 }
 
 function resetSelection(){
@@ -139,4 +140,26 @@ function addItemToCraftable(possibleItems){
 
 	};
 
+}
+
+//Tab Functionality
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
